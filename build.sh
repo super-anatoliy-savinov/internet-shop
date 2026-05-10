18e0a8489ce026473253cdf7a4150a9203a4043c
Анатолий <sasasavinoa@gmail.com>
2026-05-10 19:43:53 +0500

    Fix superuser check in build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('Anatoliy', '', 'anatoliysavinov')"
+
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='Anatoliy').exists() or User.objects.create_superuser('Anatoliy', '', 'anatoliysavinov')"
